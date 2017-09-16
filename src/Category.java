2f93c872db130d2a19fda8ad49a44ed0be1dbc67
Luke Fitzpatrick <lukedfitzpatrick@gmail.com>
2017-09-16 14:48:30 +1000

    basic structure of InterfaceDriver (more coming)

--- src/Category.java
+++ src/Category.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Category {
   private List<Task> childTasks;
-  private List<Category> childCategories;
+  private List<Category> subCategories;
   private Category parentCategory;
 
   
@@ -12,16 +12,24 @@ public class Category {
   public Category(String name) {
     this.name = name;
     childTasks = new ArrayList<Task>();
-    childCategories = new ArrayList<Category>();
+    subCategories = new ArrayList<Category>();
   }
 
+  public String getName() {
+    return name;
+  }
+  
   public void addTask(Task t) {
     childTasks.add(t);
     t.setCategory(this);
   }
 
+  public List<Task> getTasks() {
+    return childTasks;
+  }
+  
   public void addSubCategory(Category c) {
-    childCategories.add(c);
+    subCategories.add(c);
     c.setParentCategory(this);
   }
 
@@ -29,7 +37,60 @@ public class Category {
     parentCategory = c;
   }
 
+  public Category getSubCategoryByName(String uniqueName) {
+    for(Category c : subCategories) {
+      // check if it's one of our categories
+      if(c.getName().equals(uniqueName)) {
+	return c;
+      }
 
+      // now check if it's a sub category of one of the top level categories
+      else {
+	Category c2 = c.getSubCategoryByName(uniqueName);
+	if(c2 != null) {
+	  return c2;
+	}
+      }
+    }
+
+    // didn't find it
+    return null;
+
+  }
+
+  public Task getTaskByName(String uniqueName) {
+    for(Task t : childTasks) {
+      if(t.getName().equals(uniqueName)) {
+	return t;
+      }
+    }
+
+    for(Category c : subCategories) {
+      Task t = c.getTaskByName(uniqueName);
+      if(t != null) {
+	return t;
+      }
+    }
+    
+    return null;
+  }
+
+  // prints out the tasks/sub-categories etc. we have
+  public void displaySelf(String indent) {
+    System.out.println(indent + "Category '" + getName() + "'");
+
+    System.out.println(indent + "Tasks: ");
+    for(Task t : getTasks()) {
+      System.out.println(indent + "    " + t.getName());
+    }
+
+    System.out.println(indent + "Sub-categories: ");
+    for(Category c : subCategories) {
+      c.displaySelf(indent + "  ");
+    }
+  }
+
+  
   // TODO: add functions to access subtasks, do analysis etc.
 
 }
