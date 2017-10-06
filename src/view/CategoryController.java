a0f5f6dd6ba59b1c3087edc7aeea8620a54a21a6
NaomiQue <naomi.que@student.unsw.edu.au>
2017-10-07 10:03:23 +1100

    Reproducing Jackie's FXML which represents tasks and categories with (styled) buttons instead of rectangles in CategoryController. I can't seem to make a reference to the fx-id 'categoryName' in CategoryScreenFXML without breaking the rest of the code for some reason.

--- src/view/CategoryController.java
+++ src/view/CategoryController.java
@@ -2,7 +2,10 @@ package view;
 
 import javafx.fxml.FXML;
 import javafx.scene.*;
+import javafx.scene.control.Button;
 import javafx.scene.layout.TilePane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.geometry.*;
 import model.InterfaceDriver;
@@ -15,6 +18,10 @@ public class CategoryController extends Controller {
     @FXML
     private TilePane categoryTable;
 
+    /* // TO-DO: I DON'T KNOW WHY THE CODE BELOW BREAKS
+    @FXML
+    private Button categoryName;
+    */
     public CategoryController(InterfaceDriver driver, CategoryScreen currScreen, String category) {
         super(driver);
         this.currScreen = currScreen;
@@ -27,14 +34,46 @@ public class CategoryController extends Controller {
             System.out.println(cur);
         }
 
-        Node rect = new Rectangle(80,60);
-        categoryTable.getChildren().add(rect);
-        categoryTable.setMargin(rect, new Insets(22, 20, 20, 20));
+        //categoryName.setText(currCategory);
+
+
+
+        addTaskToTable("TASK");
+        addCategoryToTable("CATEGORY");
 
     }
 
     public void setCurrCategory(String category) {
         currCategory = category;
 
+    }
+
+    private Button addTaskToTable(String taskName) {
+        Button taskButton = new Button(taskName);
+        taskButton.setStyle("-fx-background-color: #939393");
+        taskButton.setTextFill(Color.WHITE);
+        taskButton.setLayoutX(30.0);
+        taskButton.setLayoutY(32.0);
+        taskButton.setPrefHeight(60.0);
+        taskButton.setPrefWidth(80.0);
+        categoryTable.getChildren().add(taskButton);
+        javafx.scene.layout.TilePane.setMargin(taskButton, new Insets(22, 20, 20, 20));
+
+        return taskButton;
+
+    }
+
+    private Button addCategoryToTable(String categoryName) {
+        Button categoryButton = new Button(categoryName);
+        categoryButton.setStyle("-fx-background-color: #3F3F3F");
+        categoryButton.setTextFill(Color.WHITE);
+        categoryButton.setLayoutX(30.0);
+        categoryButton.setLayoutY(32.0);
+        categoryButton.setPrefHeight(60.0);
+        categoryButton.setPrefWidth(80.0);
+        categoryTable.getChildren().add(categoryButton);
+        javafx.scene.layout.TilePane.setMargin(categoryButton, new Insets(22, 20, 20, 20));
+
+        return categoryButton;
     }
 }
