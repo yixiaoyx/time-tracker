9b3799515ac6d378ff2f734161e66be83af91235
jackie <jcpunkhead@gmail.com>
2017-10-08 10:08:26 +1100

    Got category menu to show ALL's subcategories

--- src/view/FormController.java
+++ src/view/FormController.java
@@ -2,6 +2,8 @@ package view;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.util.*;
 import javafx.animation.*;
 import javafx.event.*;
@@ -11,12 +13,22 @@ public class FormController extends Controller{
     private FormScreen currScreen;
     private boolean active;
 
+    @FXML
+    private MenuButton categoryMenu;
+
     public FormController(InterfaceDriver driver, FormScreen currScreen){
         super(driver);
         this.currScreen = currScreen;
 
         active = false;
 
+    }
+
+    @FXML
+    protected void initialize(){
+        for (String c: driver.getSubCategoryNames("ALL")) {
+            categoryMenu.getItems().add(new MenuItem(c));
+        }
     }
 
     @FXML
