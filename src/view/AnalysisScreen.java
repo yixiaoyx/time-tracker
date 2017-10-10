15e69da7b4fa4c1589e76c5aa3468cf5258854ef
Luke Fitzpatrick <lukedfitzpatrick@gmail.com>
2017-10-11 10:51:58 +1100

    set up classes for analysis tab

---
+++ src/view/AnalysisScreen.java
@@ -0,0 +1,20 @@
+package view;
+
+import javafx.stage.Stage;
+import model.InterfaceDriver;
+
+public class AnalysisScreen extends Screen {
+    public AnalysisScreen(Stage s, InterfaceDriver driver) {
+        super(s, "AnalysisScreen.fxml", "Analysis", driver);
+    }
+
+
+    @Override
+    Controller setUpController() {
+        AnalysisController controller = new AnalysisController(driver, this);
+        return controller;
+    }
+
+
+
+}
--- src/view/TaskController.java
+++ src/view/TaskController.java
@@ -44,7 +44,6 @@ public class TaskController extends Controller {
         );
 
         activeTime.setCycleCount(Animation.INDEFINITE);
-
     }
 
     public void setCurrTask(String currTask) {
