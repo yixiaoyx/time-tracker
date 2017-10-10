267e7efb095c69386fef96b5565ece40444d5311
winniezheng <winnie.zhengy@gmail.com>
2017-10-10 11:36:28 +1100

    Add files via upload

---
+++ database_schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS Category;
+CREATE TABLE Category (
+ID int NOT NULL AUTO_INCREMENT,
+category_name VARCHAR(50) not NULL,
+PRIMARY KEY (ID)
+);
+
+DROP TABLE IF EXISTS Tasks;
+CREATE TABLE Tasks (
+ID int NOT NULL AUTO_INCREMENT,
+task_name VARCHAR(50) not NULL,
+category_ID INT,
+duration_string VARCHAR(8) not NULL,
+duration LONG not NULL,
+date_of_task_start datetime NULL,
+date_of_task_finish datetime NULL,
+PRIMARY KEY (ID),
+FOREIGN KEY (category_ID) REFERENCES category(ID)
+);
+
