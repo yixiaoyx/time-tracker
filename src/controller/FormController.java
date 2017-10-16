0c8bb12d7e352fe7b6daa86a4d44f56e9dc700b3
jackie <jcpunkhead@gmail.com>
2017-10-17 05:46:57 +1100

    Formatted Form Screen

--- src/controller/FormController.java
+++ src/controller/FormController.java
@@ -1,4 +1,5 @@
 package controller;
+import com.jfoenix.controls.JFXComboBox;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.event.*;
@@ -8,13 +9,15 @@ public class FormController extends Controller{
     private FormScreen currScreen;
     private boolean active;
     private String selectedCategory;
+    final String topLevelCategory = "All";
+
 
     @FXML
     private ToggleGroup radioGroup;
     @FXML
     private TextField nameField;
     @FXML
-    private MenuButton categoryMenu;
+    private JFXComboBox categoryMenu;
 
     public FormController(InterfaceDriver driver, FormScreen currScreen){
         super(driver);
@@ -30,16 +33,18 @@ public class FormController extends Controller{
 
     @FXML
     protected void initialize(){
-        String topLevelCategory = "All";
+        /*
+        // Set menu
         MenuItem menuItem = new MenuItem(topLevelCategory);
         menuItem.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 setSelectedCategory(topLevelCategory);
-                categoryMenu.setText(topLevelCategory);
+
+                categoryMenu.setPromptText(topLevelCategory);
             }
         });
-        categoryMenu.setText(selectedCategory);
+        categoryMenu.setPromptText(selectedCategory);
         categoryMenu.getItems().add(menuItem);
         for (String c: driver.getSubCategoryNames(topLevelCategory)) {
             MenuItem item = new MenuItem(c);
@@ -47,11 +52,19 @@ public class FormController extends Controller{
                 @Override
                 public void handle(ActionEvent event) {
                     setSelectedCategory(c);
-                    categoryMenu.setText(c);
+                    categoryMenu.setPromptText(c);
                 }
             });
             categoryMenu.getItems().add(item);
         }
+        */
+        categoryMenu.setPromptText(selectedCategory);
+        categoryMenu.getItems().add(topLevelCategory);
+        for (String c: driver.getSubCategoryNames(topLevelCategory)){
+            categoryMenu.getItems().add(c);
+        }
+
+        //setSelectedCategory(categoryMenu.);
     }
 
     @FXML
@@ -67,7 +80,7 @@ public class FormController extends Controller{
 //        String name;
 //        String category;
        // Check selected radio group item
-        addType = radioGroup.getSelectedToggle().getUserData().toString();
+        addType = "task";//radioGroup.getSelectedToggle().getUserData().toString();
        // Check name
 
         String name = nameField.getText();
